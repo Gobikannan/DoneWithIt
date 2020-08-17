c031e3084a28f434dcd0a6be806583da8733d476
Gobikannan Shanmugam <sgobikannan@hotmail.com>
2020-08-17 22:09:52 +0100

    stack header style.

--- App.js
+++ App.js
@@ -18,12 +18,15 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerStyle: { backgroundColor: 'red' }, headerTintColor: 'white' }}>
         <Stack.Screen name="Home" component={WelcomeScreen} />
-        <Stack.Screen name="Messages" component={MessagesScreen} />
+        <Stack.Screen name="Messages" component={MessagesScreen}
+          options={{
+            title: 'Register'
+          }} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Input Form" component={InputFormScreen} />
-        <Stack.Screen name="ImagePicker" component={AppImagePicker} />
+        <Stack.Screen name="ImagePicker" component={AppImagePicker} options={({ route }) => title = route.id} />
         <Stack.Screen name="Account" component={AccountScreen} />
         <Stack.Screen name="Listings" component={ListingsScreen} />
         <Stack.Screen name="View Image" component={ViewImageScreen} />
